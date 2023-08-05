8cd4b6535fc2f9371fda729492eaaec8423f8c09
Oval_Elephant35 <principalquantum30@gmail.com>
2023-08-06 05:29:26 +0530

    Matlab update

---
+++ Conditional_Statement_4.m
@@ -0,0 +1,9 @@
+for i = 1:5
+x = 5
+y = 5
+if x>y
+    x = 5
+else
+    x = 6
+end
+end
---
+++ Important_Cmds_2.m
@@ -0,0 +1,6 @@
+x = "Important Commands"
+y = "% - To Create Comments"
+z = "%% - To Create Sections"
+a = ".m is the extension"
+b = "clear , clc ,assign"
+c = "sqrt , max , pi ,sin"
